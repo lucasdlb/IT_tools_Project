6900b89f296db695e56f54d5d05958d4072d8d05
lucasdlb <lucasdelabrosse@gmail.com>
2016-04-18 00:10:10 +0200

    Create weather_data_recuperation.py

---
+++ weather_data_recuperation.py
@@ -0,0 +1,27 @@
+#!/bin/python
+import urllib3
+import pandas as pd
+import ast
+import datetime
+import os
+
+
+http = urllib3.PoolManager()
+cities_location = pd.read_csv('/data_projet_it_tools/cities_location.csv')
+print('import cities ok')
+columns = ["ozone","time_description"]
+index = range(cities_location.shape[0])
+weather_data = pd.DataFrame(index = index, columns = columns)
+
+
+for i in index:
+    path = 'https://api.forecast.io/forecast/f7a7f0efa6e79d76fec4f6bbab71812e/%s,%s' % (cities_location.iloc[i][1], cities_location.iloc[i][2])
+    r = http.request('GET', path)
+    data = ast.literal_eval(r.data.decode('utf8'))
+    weather_data.iloc[i] = [data["currently"]["ozone"], data["currently"]["icon"]]
+file_name = 'weather_data_%s.csv' % datetime.datetime.now().strftime("%Y-%m-%d")    
+weather_data.to_csv(file_name, sep = ',', index = False)
+
+file_path = '%s/%s' % (os.getcwd(), file_name)
+print(file_path)
+os.system('hadoop fs -put %s /tmp/projet_it' % file_path)
